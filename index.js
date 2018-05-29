8f528bed38b758c646d93f0f4d3a322c819cd75d
Olena Ageyeva <eageeva@hotmail.com>
2018-05-29 11:58:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 // Code your solution in this file!
-function logDriversNames(drivers){
+function logDriverNames(drivers){
   drivers.forEach(function(driver){
     console.log(driver.name)
   })
 }
 
 function logDriversByHometown(){
-  
+
 }
 
 function driversByRevenue(){
-  
+
 }
 
 function driversByName(){
-  
+
 }
 
 function totalRevenue(){
-  
+
 }
 
 function averageRevenue(){
-  
+
 }
